40f8c5a7c3377a11ec29051d6fd289d514711f07
rs4zak <141281715+rs4zak@users.noreply.github.com>
2026-01-06 13:57:16 +0100

    ua_online.js

--- ua_online.js
+++ ua_online.js
@@ -1,13 +1,13 @@
 (function () {
     'use strict';
 
-    Lampa.Platform.tv();
+    // Використовуємо проксі для обходу блокувань на TV
+    var proxy = 'https://corsproxy.io/?';
 
     function UAOnline(object) {
         var network = new Lampa.Reguest();
         var scroll = new Lampa.Scroll({mask: true, over: true});
         var items = [];
-        var html = $('<div></div>');
         
         this.create = function () {
             var _this = this;
@@ -16,34 +16,53 @@
         };
 
         this.start = function () {
-            // Тут має бути логіка запиту до вашого проксі-сервера або API
-            // Оскільки прямі запити до сайтів заблоковані CORS
-            this.search();
-        };
-
-        this.search = function () {
-            // Приклад відображення, що пошук йде
-            this.showResults([
-                {
-                    title: "Пошук на ресурсах (UAKino, HDRezka...)",
-                    quality: "HD",
-                    url: "" 
-                }
-            ]);
-        };
-
-        this.showResults = function (data) {
             var _this = this;
-            data.forEach(function (item) {
-                var card = Lampa.Template.get('online_mod', item);
-                card.on('hover:enter', function () {
-                    Lampa.Player.play({
-                        url: item.url,
-                        title: object.movie.title
-                    });
-                });
-                scroll.append(card);
+            // Шукаємо по назві фільму на UAKino (через проксі)
+            var searchUrl = proxy + encodeURIComponent('https://uakino.me/index.php?do=search&subaction=search&story=' + object.movie.title);
+            
+            network.silent(searchUrl, function (html) {
+                if (html) {
+                    _this.parse(html);
+                } else {
+                    _this.empty();
+                }
+            }, function () {
+                _this.empty();
             });
+        };
+
+        this.parse = function (html) {
+            var _this = this;
+            var dom = $(html);
+            var results = dom.find('#dle-content .movie-item'); // Селектор для UAKino
+
+            if (results.length > 0) {
+                results.each(function () {
+                    var link = $(this).find('a').attr('href');
+                    var title = $(this).find('.movie-title').text() || object.movie.title;
+
+                    var card = Lampa.Template.get('online_mod', {
+                        title: title,
+                        quality: 'UA'
+                    });
+
+                    card.on('hover:enter', function () {
+                        // Відкриваємо сайт у плеєрі Lampa
+                        Lampa.Player.play({
+                            url: link, 
+                            title: title
+                        });
+                    });
+                    scroll.append(card);
+                });
+            } else {
+                this.empty();
+            }
+            this.activity.toggle();
+        };
+
+        this.empty = function () {
+            scroll.append(Lampa.Template.get('list_empty'));
         };
 
         this.render = function () {
@@ -51,18 +70,16 @@
         };
     }
 
-    // Додавання кнопки "Дивитись UA" у картку фільму
+    // Реєстрація кнопки в картці фільму
     Lampa.Listener.follow('full', function (e) {
         if (e.type == 'complite') {
             var btn = $('<div class="full-start__button selector"><span>Дивитись UA</span></div>');
             btn.on('hover:enter', function () {
                 Lampa.Component.add('ua_online', UAOnline);
                 Lampa.Activity.push({
-                    url: '',
                     title: 'Українською',
                     component: 'ua_online',
-                    movie: e.object.movie,
-                    page: 1
+                    movie: e.object.movie
                 });
             });
             e.body.find('.full-start__actions').append(btn);
